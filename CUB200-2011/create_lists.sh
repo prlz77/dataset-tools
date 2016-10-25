db9aa2819cc8b3e0aea17c22dbdf2e812ed4fbd6
Pau Rodriguez <prlz77@users.noreply.github.com>
2016-10-25 16:11:33 +0200

    Update create_lists.sh

--- CUB200-2011/create_lists.sh
+++ CUB200-2011/create_lists.sh
@@ -1,3 +1,3 @@
-paste images.txt image_class_labels.txt | awk '{print $2,$4}' > all.txt
+paste images.txt image_class_labels.txt | awk '{print $2,($4-1)}' > all.txt
 paste all.txt train_test_split.txt | awk '{if ($4 == 0)  print $1" "$2; }' > test.txt
 paste all.txt train_test_split.txt | awk '{if ($4 == 1)  print $1" "$2; }' > train.txt
